9046396d737b023a53227418fd870b45a1ce18d4
Richard Petit <richard-petit@live.fr>
2021-12-01 17:07:04 +0100

    rename controller and base html add

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +12,10 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -24,10 +28,11 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/logout", name="app_logout", methods={"GET"})
+     * @throws Exception
      */
     public function logout(): void
     {
         // controller can be blank: it will never be called!
-        throw new \Exception('Logout');
+        throw new Exception('Logout');
     }
 }
